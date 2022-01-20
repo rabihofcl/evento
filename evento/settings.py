fa56c4b48a135d4619ef991c6b7c50d41e7349eb
Muhammed Rabih <rabihofcl@gmail.com>
2022-01-20 21:51:21 +0530

    static files added

--- evento/settings.py
+++ evento/settings.py
@@ -154,16 +154,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+
+
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/images/'
-
-STATICFILES_DIRS = [
-    os.path.jin(BASE_DIR, 'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
 ]
 
-MEDIA_ROOT = 'static/images'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+
+# media files configuration
+
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
