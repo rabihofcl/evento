006bb425458aef3d5d323a48b425a49e6075dbfd
Muhammed Rabih <rabihofcl@gmail.com>
2022-02-08 17:07:13 +0530

    refresh token checking

--- evento/settings.py
+++ evento/settings.py
@@ -173,7 +173,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=50),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
 }
